8fda613df3cd68f26881cacbfdf7cbbcc3a8a376
Cheryl <106958690+ccp84@users.noreply.github.com>
2023-09-16 22:40:10 +0100

    complete exercise on passing child props

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Quotes from './quotes';
 import InspirationalQuote from './quote';
 import Loading from './loading';
+
 
 export type Quote = {
   id: number;
@@ -15,18 +16,19 @@ export const fetchQuotes = async (count: number) => {
 };
 
 const Application = () => {
-  const [quotes, setQuotes] = useState([]);
+  const [quotes, setQuotes] = useState<Quote[]>([]);
 
+  if (!quotes) return <Loading />
   return (
     <main className="w-full max-w-2xl py-16 mx-auto">
-      <Quotes>
-        {/* {quotes.map((quote) => (
+      <Quotes setQuotes={setQuotes}>
+        {quotes.map((quote) => (
           <InspirationalQuote
             key={quote.id}
             content={quote.content}
             source={quote.source}
           />
-        ))} */}
+        ))}
       </Quotes>
     </main>
   );
